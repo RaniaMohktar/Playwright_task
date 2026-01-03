685b76ef5d7b32a6605f81ef8ee5deff3f7383fb
Rania Mokhtar <138448849+RaniaMohktar@users.noreply.github.com>
2026-01-03 22:43:56 +0200

    Update login.spec.ts

--- tests/Tests/login.spec.ts
+++ tests/Tests/login.spec.ts
@@ -1,10 +1,10 @@
-// import { test } from '../LoginFixeture';
+import { test } from '../LoginFixeture';
 
-// test('login then click cart', async ({ LoginFixeture }) => {
+test('login then click cart', async ({ LoginFixeture }) => {
 
   
-//   await LoginFixeture.click('//*[@id="header"]/div/div/div/div[2]/div/ul/li[3]/a');
+  await LoginFixeture.click('//*[@id="header"]/div/div/div/div[2]/div/ul/li[3]/a');
 
 
-//   await LoginFixeture.waitForTimeout(5000);
-// });
+  await LoginFixeture.waitForTimeout(5000);
+});
